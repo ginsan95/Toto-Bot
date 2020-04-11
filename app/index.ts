7767e4ca1e26c40d6983c5f1aabe9296a14cf1db
Avery Choke <averychokedev@gmail.com>
2020-04-11 22:19:46 +0800

    feat: Add a server to prevent crash at Heroku

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,7 @@ import { CronJob } from 'cron';
 import { parse as htmlParse } from 'node-html-parser';
 import { parse as dateParse, format as dateFormat } from 'date-fns';
 import { JWT } from 'google-auth-library';
+import { createServer } from 'https';
 import * as api from './api';
 import constants from './constants';
 
@@ -127,5 +128,10 @@ console.log(`Push notification when money >= ${constants.minMoney}`);
 
 console.log('Constant values are');
 console.log(constants);
+
+createServer({}, (req, res) => {
+    res.writeHead(200);
+    res.end('Toto Bot');
+}).listen(8000);
 
 job.start();
